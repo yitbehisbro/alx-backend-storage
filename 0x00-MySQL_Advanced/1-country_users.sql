573d9d0a2c46bbca859e1a2c6e62aae03b966f7d
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-05-03 15:39:58 +0000

    Creates a table users with id, email, name and country with enumuration value 'US, CO and TN' and default value US

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Adding enumuration coulmn
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
