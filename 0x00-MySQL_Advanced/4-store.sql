2a35b5d3b90d3f15666fb61285f8bb10031b5474
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-05-04 10:06:51 +0000

    Error fixed toward the checker

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,7 +5,7 @@ AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - orders.number
-    WHERE name = orders.item_name;
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
 END//
 DELIMITER ;
