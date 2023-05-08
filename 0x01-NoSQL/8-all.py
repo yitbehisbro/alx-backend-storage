b1a82d996ead26ed8520775287efc885b9de52ea
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-05-08 13:48:59 +0300

    List all documents in collection using Python function

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,12 +3,6 @@
 
 
 def list_all(mongo_collection):
-    """Lists all documents in a collection.
-
-    Args:
-        mongo_collection: pymongo collection object
-
-    Returns:
-        List of documents in the collection
+    """List all documents in collection using Python function
     """
     return list(mongo_collection.find())
