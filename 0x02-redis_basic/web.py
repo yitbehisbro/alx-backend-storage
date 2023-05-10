deb4ca8a32bf043c1aeda09ceb1372ed520240a3
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-05-11 00:24:29 +0300

    Create web.py
    Implementing an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import requests
+from redis import Redis
+
+redis = Redis()
+
+def get_page(url: str) -> str:
+    """ Implementing an expiring web cache and tracker """
+    count_key = f'count:{url}'
+    cache_key = f'cache:{url}'
+    redis.incr(count_key)
+    content = redis.get(cache_key)
+    if content is None:
+        response = requests.get(url)
+        content = response.text
+        redis.setex(cache_key, 10, content)
+    return content
