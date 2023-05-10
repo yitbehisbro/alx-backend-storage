8516f932505b5905ba878e61e8d52bc18ad78d02
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-05-11 00:02:04 +0300

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,7 @@ from redis import Redis
 from typing import Union
 UnionOfTypes = Union[str, bytes, int, float]
 
+
 class Cache:
     """ Writing strings to Redis """
     def __init__(self):
@@ -12,8 +13,30 @@ class Cache:
         self._redis = Redis()
         self._redis.flushdb()
 
+
     def store(self, data: UnionOfTypes) -> str:
         """ Takes a data argument and returns a string """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> UnionOfTypes:
+        """  convert the data back to the desired format. """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            data = fn(data)
+        return data
+
+
+    def get_str(self, key: str) -> Optional[str]:
+        """ automatically parametrize Cache.get with the
+        correct conversion function. """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+
+    def get_int(self, key: str) -> Optional[int]:
+        """ automatically parametrize Cache.get with the
+        correct conversion function. """
+        return self.get(key, fn=int)
